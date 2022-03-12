25987f13b0f05c00a9a6b2b5acdaa83bf720e1c7
itsf4llofstars <irooted4hal@mailfence.com>
2022-03-12 13:00:26 -0600

    Add build rotor function

--- build-rotors.py
+++ build-rotors.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 """create-rotors.py file"""
+import random
+
+
+letter_list = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+
+
+def build_rotor(rotor_list):
+    while len(rotor_list) < 26:
+        rotor_letter = random.choice(letter_list)
+        if rotor_letter in rotor_list:
+            continue
+        elif rotor_letter not in rotor_list:
+            rotor_list.append(rotor_letter)
 
 
 def main():
-    pass
+    rotor_i = []
+    build_rotor(rotor_i)
 
 
 if __name__ == "__main__":
