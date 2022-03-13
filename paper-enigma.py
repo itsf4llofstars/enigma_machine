fbb32ff821773ec1dbe4e4934d66a1910961c70c
itsf4llofstars <irooted4hal@mailfence.com>
2022-03-13 12:45:46 -0500

    Add fix repr

--- paper-enigma.py
+++ paper-enigma.py
@@ -18,34 +18,41 @@ class PaperEnigma:
         self.reflector = deque(["A", "B", "C", "D", "E", "F", "G", "D", "I", "J", "K", "G", "M", "K", "M", "I", "E", "B", "F", "T", "C", "V", "V", "J", "A", "T"])
 
         self.rotors = {
-            ["left"]: [self.in_out,[]],
-            ["center"]: [self.in_out, []],
-            ["right"]: [self.in_out, []],
+            "left_in": deque(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
+            "left_out": deque(["E", "K", "M", "F", "L", "G", "D", "Q", "V", "Z", "N", "T", "O", "W", "Y", "H", "X", "U", "S", "P", "A", "I", "B", "R", "C", "J", "E", "K", "M", "F", "L", "G", "D", "Q", "V", "Z", "N", "T", "O", "W", "Y", "H", "X", "U", "S", "P", "A", "I", "B", "R", "C", "J"]),
+            "center_in": deque(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
+            "center_out": deque(["A", "J", "D", "K", "S", "I", "R", "U", "X", "B", "L", "H", "W", "T", "M", "C", "Q", "G", "Z", "N", "P", "Y", "F", "V", "O", "E", "A", "J", "D", "K", "S", "I", "R", "U", "X", "B", "L", "H", "W", "T", "M", "C", "Q", "G", "Z", "N", "P", "Y", "F", "V", "O", "E"]),
+            "right_in": deque(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
+            "right_out": deque(["B", "D", "F", "H", "J", "L", "C", "P", "R", "T", "X", "V", "Z", "N", "Y", "E", "I", "W", "G", "A", "K", "M", "U", "S", "Q", "O", "B", "D", "F", "H", "J", "L", "C", "P", "R", "T", "X", "V", "Z", "N", "Y", "E", "I", "W", "G", "A", "K", "M", "U", "S", "Q", "O"]),
         }
 
         self.days_rotors = days_rotors
 
     def __repr__(self):
-        rotors_string = f"{self.rotors['left'][0]}\n{self.rotors['left'][1]}\n\n{self.rotors['center'][0]}\n{self.rotors['center'][1]}\n\n{self.rotors['right'][0]}\n{self.rotors['right'][1]}"
+        rotors_string = f"{self.rotors['left_in']}\n{self.rotors['left_out']}\n\n{self.rotors['center_in']}\n{self.rotors['center_out']}\n\n{self.rotors['right_in']}\n{self.rotors['right_out']}"
+        return rotors_string
+
+    def print_rotors():
+        pass
 
     def set_rotors(self):
-        if days_rotors[0] == "I":
+        if self.days_rotors[0] == "I":
             self.rotors["left"][1] = self.rotor_i_in
-        elif days_rotors[0] == "II":
+        elif self.days_rotors[0] == "II":
             self.rotors["left"][1] = self.rotor_ii_in
         else:
             self.rotors["left"][1] = self.rotor_iii_in
 
-        if days_rotors[1] == "I":
+        if self.days_rotors[1] == "I":
             self.rotors["center"][1] = self.rotor_i_in
-        elif days_rotors[1] == "II":
+        elif self.days_rotors[1] == "II":
             self.rotors["center"][1] = self.rotor_ii_in
         else:
             self.rotors["center"][1] = self.rotor_iii_in
 
-        if days_rotors[2] == "I":
+        if self.days_rotors[2] == "I":
             self.rotors["right"][1] = self.rotor_i_in
-        elif days_rotors[2] == "II":
+        elif self.days_rotors[2] == "II":
             self.rotors["right"][1] = self.rotor_ii_in
         else:
             self.rotors["right"][1] = self.rotor_iii_in
@@ -53,7 +60,6 @@ class PaperEnigma:
 
 def main():
     enigma = PaperEnigma(["I", "II", "III"])
-    enigma.set_rotors()
     print(enigma)
 
 
