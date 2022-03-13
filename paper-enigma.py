def772629e6a3eee729953f1f4a7ced47647156d
itsf4llofstars <irooted4hal@mailfence.com>
2022-03-13 12:18:05 -0500

    Add set rotors method

--- paper-enigma.py
+++ paper-enigma.py
@@ -3,7 +3,7 @@ from collections import deque
 
 
 class PaperEnigma:
-    def __init__(self):
+    def __init__(self, days_rotors):
         self.in_out = deque(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
 
         self.rotor_i_out = deque(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
@@ -16,3 +16,34 @@ class PaperEnigma:
         self.rotor_iii_in = deque(['B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 'U', 'S', 'Q', 'O', 'B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 'U', 'S', 'Q', 'O'])
 
         self.reflector = deque(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'D', 'I', 'J', 'K', 'G', 'M', 'K', 'M', 'I', 'E', 'B', 'F', 'T', 'C', 'V', 'V', 'J', 'A', 'T'])
+
+        self.rotors {
+            ['left']: [self.in_out,[]],
+            ['center']: [self.in_out, []],
+            ['right']: [self.in_out, []],
+        }
+
+        self.days_rotors = days_rotors
+
+        def set_rotors(self):
+            if days_rotors[0] == 'I':
+                self.rotors['left'][1] = self.rotor_i_in
+            elif days_rotors[0] == 'II':
+                self.rotors['left'][1] = self.rotor_ii_in
+            else:
+                self.rotors['left'][1] = self.rotor_iii_in
+
+            if days_rotors[1] == 'I':
+                self.rotors['center'][1] = self.rotor_i_in
+            elif days_rotors[1] == 'II':
+                self.rotors['center'][1] = self.rotor_ii_in
+            else:
+                self.rotors['center'][1] = self.rotor_iii_in
+
+            if days_rotors[2] == 'I':
+                self.rotors['right'][1] = self.rotor_i_in
+            elif days_rotors[2] == 'II':
+                self.rotors['right'][1] = self.rotor_ii_in
+            else:
+                self.rotors['right'][1] = self.rotor_iii_in
+
