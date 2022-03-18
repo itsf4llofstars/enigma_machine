1c5681e19dcfc50ac22c18a1343fe4accc46daeb
itsf4llofstars <irooted4hal@mailfence.com>
2022-03-18 14:28:32 -0500

    Add set rotor to key

--- paper-enigma.py
+++ paper-enigma.py
@@ -3,7 +3,7 @@ from collections import deque
 
 
 class PaperEnigma:
-    def __init__(self, days_rotors):
+    def __init__(self, days_rotors, key):
         self.in_out = deque(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"])
 
         self.rotor_i_out = deque(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"])
@@ -18,21 +18,23 @@ class PaperEnigma:
         self.reflector = deque(["A", "B", "C", "D", "E", "F", "G", "D", "I", "J", "K", "G", "M", "K", "M", "I", "E", "B", "F", "T", "C", "V", "V", "J", "A", "T"])
 
         self.rotors = {
-            "left_in": deque(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
-            "left_out": deque(["E", "K", "M", "F", "L", "G", "D", "Q", "V", "Z", "N", "T", "O", "W", "Y", "H", "X", "U", "S", "P", "A", "I", "B", "R", "C", "J", "E", "K", "M", "F", "L", "G", "D", "Q", "V", "Z", "N", "T", "O", "W", "Y", "H", "X", "U", "S", "P", "A", "I", "B", "R", "C", "J"]),
-            "center_in": deque(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
-            "center_out": deque(["A", "J", "D", "K", "S", "I", "R", "U", "X", "B", "L", "H", "W", "T", "M", "C", "Q", "G", "Z", "N", "P", "Y", "F", "V", "O", "E", "A", "J", "D", "K", "S", "I", "R", "U", "X", "B", "L", "H", "W", "T", "M", "C", "Q", "G", "Z", "N", "P", "Y", "F", "V", "O", "E"]),
-            "right_in": deque(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
-            "right_out": deque(["B", "D", "F", "H", "J", "L", "C", "P", "R", "T", "X", "V", "Z", "N", "Y", "E", "I", "W", "G", "A", "K", "M", "U", "S", "Q", "O", "B", "D", "F", "H", "J", "L", "C", "P", "R", "T", "X", "V", "Z", "N", "Y", "E", "I", "W", "G", "A", "K", "M", "U", "S", "Q", "O"]),
+            "left_in": deque(["E", "K", "M", "F", "L", "G", "D", "Q", "V", "Z", "N", "T", "O", "W", "Y", "H", "X", "U", "S", "P", "A", "I", "B", "R", "C", "J", "E", "K", "M", "F", "L", "G", "D", "Q", "V", "Z", "N", "T", "O", "W", "Y", "H", "X", "U", "S", "P", "A", "I", "B", "R", "C", "J"]),
+            "left_out": deque(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
+            "center_in": deque(["A", "J", "D", "K", "S", "I", "R", "U", "X", "B", "L", "H", "W", "T", "M", "C", "Q", "G", "Z", "N", "P", "Y", "F", "V", "O", "E", "A", "J", "D", "K", "S", "I", "R", "U", "X", "B", "L", "H", "W", "T", "M", "C", "Q", "G", "Z", "N", "P", "Y", "F", "V", "O", "E"]),
+            "center_out": deque(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
+            "right_in": deque(["B", "D", "F", "H", "J", "L", "C", "P", "R", "T", "X", "V", "Z", "N", "Y", "E", "I", "W", "G", "A", "K", "M", "U", "S", "Q", "O", "B", "D", "F", "H", "J", "L", "C", "P", "R", "T", "X", "V", "Z", "N", "Y", "E", "I", "W", "G", "A", "K", "M", "U", "S", "Q", "O"]),
+            "right_out": deque(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
         }
 
         self.days_rotors = days_rotors
+        self.key = key.upper()
 
     def __repr__(self):
         rotors_string = f"{self.rotors['left_in']}\n{self.rotors['left_out']}\n\n{self.rotors['center_in']}\n{self.rotors['center_out']}\n\n{self.rotors['right_in']}\n{self.rotors['right_out']}"
         return rotors_string
 
     def print_left(self):
+        print("Left Rotor")
         for letter in self.rotors["left_in"]:
             print(letter, end=" ")
         print()
@@ -42,6 +44,7 @@ class PaperEnigma:
         print()
 
     def print_center(self):
+        print("Center Rotor")
         for letter in self.rotors["center_in"]:
             print(letter, end=" ")
         print()
@@ -51,6 +54,7 @@ class PaperEnigma:
         print()
 
     def print_right(self):
+        print("Right Rotor")
         for letter in self.rotors["right_in"]:
             print(letter, end=" ")
         print()
@@ -59,27 +63,18 @@ class PaperEnigma:
         print()
         print()
 
-    def set_rotors(self):
-        if self.days_rotors[0] == "I":
-            self.rotors["left"][1] = self.rotor_i_in
-        elif self.days_rotors[0] == "II":
-            self.rotors["left"][1] = self.rotor_ii_in
-        else:
-            self.rotors["left"][1] = self.rotor_iii_in
+    def set_rotor_key(self):
+        while self.rotors["left_out"][0] != self.key[0]:
+            deque.rotate(self.rotors["left_in"])
+            deque.rotate(self.rotors["left_out"])
 
-        if self.days_rotors[1] == "I":
-            self.rotors["center"][1] = self.rotor_i_in
-        elif self.days_rotors[1] == "II":
-            self.rotors["center"][1] = self.rotor_ii_in
-        else:
-            self.rotors["center"][1] = self.rotor_iii_in
+        while self.rotors["center_out"][0] != self.key[1]:
+            deque.rotate(self.rotors["center_in"])
+            deque.rotate(self.rotors["center_out"])
 
-        if self.days_rotors[2] == "I":
-            self.rotors["right"][1] = self.rotor_i_in
-        elif self.days_rotors[2] == "II":
-            self.rotors["right"][1] = self.rotor_ii_in
-        else:
-            self.rotors["right"][1] = self.rotor_iii_in
+        while self.rotors["right_out"][0] != self.key[2]:
+            deque.rotate(self.rotors["right_in"])
+            deque.rotate(self.rotors["right_out"])
 
     def rotate_left(self, n=-1):
         deque.rotate(self.rotors["left_in"], n)
@@ -101,36 +96,18 @@ class PaperEnigma:
 
 
 def main():
-    enigma = PaperEnigma(["I", "II", "III"])
-
-    for _ in range(10):
-        enigma.rotate_left()
-        enigma.rotate_right()
-    for _ in range(2):
-        enigma.rotate_center()
-        enigma.rotate_right()
-
-    enigma.rotate_left()
-
-    enigma.print_left()
-    enigma.print_center()
+    enigma = PaperEnigma(["I", "II", "III"], 'mck')
+    enigma.set_rotor_key()
     enigma.print_right()
+
+    enigma.rotate_right()
+    enigma.print_right()
+    enigma.print_center()
+    enigma.print_left()
+
     print("-" * 80)
 
     letter = "E"
-
-    index_of_letter = enigma.get_index_of_letter(enigma.rotors["left_in"], letter)
-    letter = enigma.get_letter_at_index(enigma.rotors["left_out"], index_of_letter)
-    print(index_of_letter, letter)
-
-    index_of_letter = enigma.get_index_of_letter(enigma.rotors["left_in"], letter)
-    letter = enigma.get_letter_at_index(enigma.rotors["left_out"], index_of_letter)
-    print(index_of_letter, letter)
-
-    index_of_letter = enigma.get_index_of_letter(enigma.rotors["left_in"], letter)
-    letter = enigma.get_letter_at_index(enigma.rotors["left_out"], index_of_letter)
-
-    print(index_of_letter, letter)
 
 
 if __name__ == "__main__":
