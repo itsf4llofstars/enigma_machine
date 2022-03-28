7a4ac1da9c34afb3d97c4d617c1bd17f2b6b0327
itsf4llofstars <irooted4hal@mailfence.com>
2022-03-27 23:12:50 -0500

    Add reflector calls

--- build-rotors.py
+++ build-rotors.py
@@ -73,18 +73,24 @@ def main():
     rotor_iii = []
     rotor_iv = []
     rotor_v = []
+    reflectorB = []
+    reflectorC = []
 
     build_rotor(rotor_i)
     build_rotor(rotor_ii)
     build_rotor(rotor_iii)
     build_rotor(rotor_iv)
     build_rotor(rotor_v)
+    build_reflector(reflectorB)
+    build_reflector(reflectorC)
 
     shuffle_rotor(rotor_i, 1)
     shuffle_rotor(rotor_ii, 1)
     shuffle_rotor(rotor_iii, 1)
     shuffle_rotor(rotor_iv, 1)
     shuffle_rotor(rotor_v, 1)
+    shuffle_rotor(reflectorB)
+    shuffle_rotor(reflectorC)
 
     filepath = "rotors.txt"  # Should be in the working directory
     check_for_rotor_file(filepath)
@@ -94,8 +100,10 @@ def main():
     write_rotor_file(rotor_iii)
     write_rotor_file(rotor_iv)
     write_rotor_file(rotor_v)
+    write_rotor_file(reflectorB)
+    write_rotor_file(reflectorC)
 
-    print("New Rotors built")
+    print("New Rotors and reflectors built")
 
     # print(rotor_i)
     # print(rotor_ii)
