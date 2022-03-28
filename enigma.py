aa0eb853d843ab93cdd8d7868dc6bac905ac4243
itsf4llofstars <irooted4hal@mailfence.com>
2022-03-27 23:52:38 -0500

    Add stored rotors dict
    created enigma machine

--- enigma.py
+++ enigma.py
@@ -29,14 +29,23 @@ from collections import deque
 class Enigma:
     """Class model of the German Enigma Test Encoding machine
     """
-    def __init__(self, rotors, key) -> None:
+    def __init__(self, rotors, rings: str, key: str) -> None:
         self.keyboard = deque(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"])
 
+        # May not be needed
         self.rotor1 = deque(["U", "L", "R", "X", "C", "Q", "O", "K", "G", "N", "J", "A", "F", "I", "H", "Z", "W", "S", "E", "Y", "M", "P", "D", "T", "B", "V"])
-        self.rotor1 = deque(["Y", "T", "K", "F", "J", "M", "D", "Q", "C", "H", "X", "R", "P", "E", "L", "W", "Z", "N", "V", "I", "S", "O", "G", "B", "U", "A"])
-        self.rotor1 = deque(["P", "X", "E", "Z", "B", "W", "Y", "L", "F", "G", "U", "C", "D", "S", "I", "R", "J", "O", "M", "A", "K", "H", "T", "N", "V", "Q"])
-        self.rotor1 = deque(["R", "P", "X", "G", "K", "S", "B", "Y", "E", "C", "J", "I", "H", "U", "T", "N", "D", "M", "F", "Q", "A", "V", "L", "Z", "O", "W"])
-        self.rotor1 = deque(["S", "M", "I", "R", "X", "P", "J", "U", "O", "A", "V", "D", "Q", "F", "L", "C", "Z", "K", "T", "W", "G", "E", "N", "B", "H", "Y"])
+        self.rotor2 = deque(["Y", "T", "K", "F", "J", "M", "D", "Q", "C", "H", "X", "R", "P", "E", "L", "W", "Z", "N", "V", "I", "S", "O", "G", "B", "U", "A"])
+        self.rotor3 = deque(["P", "X", "E", "Z", "B", "W", "Y", "L", "F", "G", "U", "C", "D", "S", "I", "R", "J", "O", "M", "A", "K", "H", "T", "N", "V", "Q"])
+        self.rotor4 = deque(["R", "P", "X", "G", "K", "S", "B", "Y", "E", "C", "J", "I", "H", "U", "T", "N", "D", "M", "F", "Q", "A", "V", "L", "Z", "O", "W"])
+        self.rotor5 = deque(["S", "M", "I", "R", "X", "P", "J", "U", "O", "A", "V", "D", "Q", "F", "L", "C", "Z", "K", "T", "W", "G", "E", "N", "B", "H", "Y"])
+
+        self.stored_rotors = {
+            "I": deque(["U", "L", "R", "X", "C", "Q", "O", "K", "G", "N", "J", "A", "F", "I", "H", "Z", "W", "S", "E", "Y", "M", "P", "D", "T", "B", "V"]),
+            "II": deque(["Y", "T", "K", "F", "J", "M", "D", "Q", "C", "H", "X", "R", "P", "E", "L", "W", "Z", "N", "V", "I", "S", "O", "G", "B", "U", "A"]),
+            "III": deque(["P", "X", "E", "Z", "B", "W", "Y", "L", "F", "G", "U", "C", "D", "S", "I", "R", "J", "O", "M", "A", "K", "H", "T", "N", "V", "Q"]),
+            "IV": deque(["R", "P", "X", "G", "K", "S", "B", "Y", "E", "C", "J", "I", "H", "U", "T", "N", "D", "M", "F", "Q", "A", "V", "L", "Z", "O", "W"]),
+            "V": deque(["S", "M", "I", "R", "X", "P", "J", "U", "O", "A", "V", "D", "Q", "F", "L", "C", "Z", "K", "T", "W", "G", "E", "N", "B", "H", "Y"]),
+        }
 
         self.rotors = {
             "right_input": deque(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
@@ -50,10 +59,12 @@ class Enigma:
         self.reflectorB = ["F", "B", "A", "V", "B", "A", "T", "Q", "K", "P", "H", "F", "T", "U", "Q", "P", "J", "X", "J", "K", "V", "X", "Y", "U", "H", "Y"]
         self.reflectorC = ["E", "W", "Z", "F", "N", "W", "Q", "O", "E", "Z", "Y", "K", "V", "D", "L", "R", "D", "K", "N", "O", "Q", "V", "Y", "R", "F", "L"]
 
+        self.ring_setting = rings.upper()
+        self.key_setting = key.upper()
+
 def main():
-    pass
+    enigma = Enigma(["I", "II", "III"], "ABC", "XYZ")
 
 
 if __name__ == "__main__":
     main()
-
