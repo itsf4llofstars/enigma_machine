439cea4272710e2463bf6125a1a459032ed09b51
itsf4llofstars <irooted4hal@mailfence.com>
2022-04-15 13:47:00 -0500

    Add example string
    String prints

--- main.py
+++ main.py
@@ -33,73 +33,81 @@ from enigma import Enigma
 # rotors = f.get_rotors()
 # letter = f.get_user_letter()
 
-reflector = "B"
-ring = "ABC"
-key = "XYZ"
-rotors = ["I", "II", "III"]
-letter = f.get_user_letter()
+if 0:
+    reflector = "B"
+    ring = "ABC"
+    key = "XYZ"
+    rotors = ["I", "II", "III"]
+    letter = f.get_user_letter()
 
-print(f"{reflector = }")
-print(f"{ring = }")
-print(f"{key = }")
-print(f"{rotors = }")
-print(f"{letter = }")
+    print(f"{reflector = }")
+    print(f"{ring = }")
+    print(f"{key = }")
+    print(f"{rotors = }")
+    print(f"{letter = }")
 
 # The below four calls must be in their current order
-enigma = Enigma(reflector, rotors, ring, key)  # 1
-enigma.set_rotors()  # 2
-enigma.set_rings()  # 3
-enigma.set_key()  # 4
+    enigma = Enigma(reflector, rotors, ring, key)  # 1
+    enigma.set_rotors()  # 2
+    enigma.set_rings()  # 3
+    enigma.set_key()  # 4
 
 
-go = True
-user_input_letter = ""
-while go:
-    letter = f.get_user_letter()
-    if letter == "QQ":
-        go = False
-        continue
-    else:
-        user_input_letter = letter
+    go = True
+    user_input_letter = ""
+    while go:
+        letter = f.get_user_letter()
+        if letter == "QQ":
+            go = False
+            continue
+        else:
+            user_input_letter = letter
 
-    enigma.rotate_rotor_right()
+        enigma.rotate_rotor_right()
 
-    # enigma.show_rotors()
+        # enigma.show_rotors()
 
-    index = enigma.get_index_of_letter(enigma.keyboard, letter)
-    letter = enigma.get_letter_at_index(enigma.rotors["right_input"], index)
-    index = enigma.get_rotor_output_index(enigma.rotors["right_output"], letter)
+        index = enigma.get_index_of_letter(enigma.keyboard, letter)
+        letter = enigma.get_letter_at_index(enigma.rotors["right_input"], index)
+        index = enigma.get_rotor_output_index(enigma.rotors["right_output"], letter)
 
-    # Center Rotor
-    letter = enigma.get_letter_at_index(enigma.rotors["center_input"], index)
-    index = enigma.get_rotor_output_index(enigma.rotors["center_output"], letter)
+        # Center Rotor
+        letter = enigma.get_letter_at_index(enigma.rotors["center_input"], index)
+        index = enigma.get_rotor_output_index(enigma.rotors["center_output"], letter)
 
-    # Left rotor
-    letter = enigma.get_letter_at_index(enigma.rotors["left_input"], index)
-    index = enigma.get_rotor_output_index(enigma.rotors["left_output"], letter)
+        # Left rotor
+        letter = enigma.get_letter_at_index(enigma.rotors["left_input"], index)
+        index = enigma.get_rotor_output_index(enigma.rotors["left_output"], letter)
 
-    # Reflector
-    reflector_in_letter = enigma.get_letter_at_index(enigma.reflector, index)
-    reflector_out_index = enigma.get_reflector_out_index(
-        enigma.reflector, index, reflector_in_letter
-    )
+        # Reflector
+        reflector_in_letter = enigma.get_letter_at_index(enigma.reflector, index)
+        reflector_out_index = enigma.get_reflector_out_index(
+            enigma.reflector, index, reflector_in_letter
+        )
 
-    # Left rotor
-    letter = enigma.get_letter_at_index(
-        enigma.rotors["left_output"], reflector_out_index
-    )
-    index = enigma.get_index_of_letter(enigma.rotors["left_input"], letter)
+        # Left rotor
+        letter = enigma.get_letter_at_index(
+            enigma.rotors["left_output"], reflector_out_index
+        )
+        index = enigma.get_index_of_letter(enigma.rotors["left_input"], letter)
 
-    # Center rotor
-    letter = enigma.get_letter_at_index(enigma.rotors["center_output"], index)
-    index = enigma.get_index_of_letter(enigma.rotors["center_input"], letter)
+        # Center rotor
+        letter = enigma.get_letter_at_index(enigma.rotors["center_output"], index)
+        index = enigma.get_index_of_letter(enigma.rotors["center_input"], letter)
 
-    # Rigth rotor
-    letter = enigma.get_letter_at_index(enigma.rotors["right_output"], index)
-    index = enigma.get_index_of_letter(enigma.rotors["right_input"], letter)
+        # Rigth rotor
+        letter = enigma.get_letter_at_index(enigma.rotors["right_output"], index)
+        index = enigma.get_index_of_letter(enigma.rotors["right_input"], letter)
 
-    # Encoded letter
-    encoded_letter = enigma.get_letter_at_index(enigma.keyboard, index)
-    print(f"Input: {user_input_letter} -> {encoded_letter}")
+        # Encoded letter
+        encoded_letter = enigma.get_letter_at_index(enigma.keyboard, index)
+        print(f"Input: {user_input_letter} -> {encoded_letter}")
 
-print(f"User quit")
+    print(f"User quit")
+
+example_str = "THEXEASTXMOONXRISESXWITHXNOXTIDEX"
+
+for letter in example_str:
+    print(letter, end=" ")
+print()
+
