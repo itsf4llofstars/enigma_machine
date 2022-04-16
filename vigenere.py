b55e33097d7dce4bf8d9d9d3b9c25b440f087cf7
itsf4llofstars <irooted4hal@mailfence.com>
2022-04-15 22:30:31 -0500

    Add get and encode message functions

--- vigenere.py
+++ vigenere.py
@@ -36,19 +36,49 @@ def set_key_letter(letter: str) -> None:
         rotor_out.append(temp_letter)
 
 
-def get_message():
-    """
+def get_message() -> str:
+    """Gets the users message and retruns it as a string
+
+    return:
+        message [str]: Users inputted message
     """
     message = ""
-    while message len(message) == 0:
+    while len(message) == 0:
         message: str = str(input("Enter your message: ").upper())
         if len(message) < 1:
+            print("Please enter a message longer than 0\n")
+    return message
+
+
+def encode_message(message: str, key: str):
+    """Encodeds users message
+
+    return:
+        ecoded_message [list: str]: Users message encoded
+    """
+    key_list = list(key)
+    encoded_message = []
+    index = 0
+    while index < len(message):
+        while rotor_out[0] != key_list[0]:
+            temp = rotor_out.pop(0)
+            rotor_out.append(temp)
+        letter_index = rotor_in.index(message[index])
+        encoded_letter = rotor_out[letter_index]
+        encoded_message.append(encoded_letter)
+        index += 1
+    return encoded_message
 
 
 user_key: str = get_key()
-set_key_letter(user_key[0])
+# set_key_letter(user_key[0])
+user_message: str = get_message()
+user_encoded_message = encode_message(user_message, user_key)
 
 print(user_key)
 print(rotor_in)
 print(rotor_out)
+print(user_message)
+[print(letter, end="") for letter in user_encoded_message]
+print()
 
