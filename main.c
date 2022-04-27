f1aec3f14f0a540a02870a571085cd8cea43090c
itsf4llofstars <irooted4hal@mailfence.com>
2022-04-26 23:22:12 -0500

    Add getIndex and getLetter function

--- main.c
+++ main.c
@@ -20,8 +20,17 @@ void printRotor(char rotor[])
         printf("%c ", rotor[i]);
 }
 
-// getIndex
-// getLetter
+int getIndex(char rotor[], char letter) {
+    for (int i = 0; i < 25; ++i)
+        if (rotor[i] == letter)
+            return i;
+
+    return -1;
+}
+
+char getLetter(char rotor[], int index) {
+    return rotor[index];
+}
 
 int main(int argc, char **argv)
 {
@@ -31,20 +40,53 @@ int main(int argc, char **argv)
     };
 
     char rotorRightIn[] = {
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'A', 'B', 'C', 'E', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
         'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
     };
+    char rotorRightOut[] = {
+        'S', 'N', 'M', 'Y', 'K', 'W', 'H', 'Q', 'L', 'F', 'C', 'O', 'E',
+        'T', 'G', 'Z', 'D', 'B', 'A', 'I', 'V', 'U', 'J', 'X', 'P', 'R'
+    };
+
     char rotorMiddleIn[] = {
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
         'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
     };
+    char rotorMiddleOut[] = {
+        'Y', 'E', 'J', 'S', 'P', 'X', 'A', 'W', 'H', 'O', 'D', 'L', 'F',
+        'Q', 'G', 'K', 'B', 'V', 'U', 'I', 'Z', 'N', 'T', 'C', 'R', 'M'
+    };
+
     char rotorLeftIn[] = {
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
         'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
     };
+    char rotorLeftOut[] = {
+        'J', 'Y', 'V', 'P', 'X', 'R', 'U', 'I', 'L', 'O', 'H', 'B', 'S',
+        'Q', 'G', 'F', 'C', 'A', 'M', 'W', 'T', 'D', 'Z', 'K', 'N', 'E'
+    };
 
-    rotateRotor(rotorRightIn, 5);
+    char rightRotor[2][26] = {
+    {
+        'A', 'B', 'C', 'E', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    },
+    {
+        'S', 'N', 'M', 'Y', 'K', 'W', 'H', 'Q', 'L', 'F', 'C', 'O', 'E',
+        'T', 'G', 'Z', 'D', 'B', 'A', 'I', 'V', 'U', 'J', 'X', 'P', 'R'
+    }};
+
+    printRotor(keyboard);
+    puts("");
+
+    char letter = 'D';
+    int index;
+    index = getIndex(keyboard, letter);
+    letter = getLetter(rotorRightIn, index);
+    printf("%c - %d\n", letter, index);
     printRotor(rotorRightIn);
+
+
 
     puts("");
     return 0;
