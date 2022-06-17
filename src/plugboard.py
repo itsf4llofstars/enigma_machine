73461b9c30ae7b77b0c6c23fca13ba0ff33a9465
itsf4llofstars <irooted4hal@mailfence.com>
2022-06-17 13:52:33 -0500

    Add black format inhibit

--- src/plugboard.py
+++ src/plugboard.py
@@ -1,9 +1,11 @@
 """Plug Board Test Script"""
 
+# fmt: off
 letters = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 ]
+# fmt: on
 
 plugboard = []
 plugstring = ''
